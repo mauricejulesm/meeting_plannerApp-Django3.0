90db8adb707aab9aff85d6d02a51650a99e6c93c
Maurice JM <julesmaurice.mulisa@gmail.com>
2020-04-26 22:40:00 +0200

    configured error page

--- meetings/views.py
+++ meetings/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Meeting
 
 
+# a function to show the meeting details.
 def details(request, id):
-    meeting = Meeting.objects.get(pk=id)
+    meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/details.html", {"meeting": meeting})
